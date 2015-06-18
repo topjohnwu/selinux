f33298730c2535fc791a8908f79dacad8b28b1d8
Jeff Vander Stoep <jeffv@google.com>
2015-06-18 08:11:40 -0700

    Move python libraries to prebuilt directory
    Update script syntax to work with sh/dash/bash
    
    Add cleanspec
    
    (Cherry picked from 8a2e0bafc9c56de823d8fe7e52ca6f483b4ce011)
    
    Bug:21928182
    Change-Id: Ifc19252d8d8e178b86c51fe1f54e162a61ffc0dd

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,2 +1,5 @@
 # This empty CleanSpec.mk file will prevent the build system
 # from descending into subdirs.
+#
+$(call add-clean-step, rm -rf $(OUT_DIR)/host/linux-x86/bin/audit2allow)
+$(call add-clean-step, rm -rf $(OUT_DIR)/host/linux-x86/bin/audit2why)
