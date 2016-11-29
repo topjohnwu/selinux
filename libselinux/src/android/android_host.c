715ff04e5c1d498e56adc658c1442ccbb48cc2f1
Nick Kralevich <nnk@google.com>
2016-11-29 01:07:55 +0000

    Revert "Delete "libselinux.so.1""
    The mac build is broken. Reverting while I figure out an appropriate solution.
    
    external/selinux/libselinux/src/avc_internal.c:21:10: fatal error: 'linux/types.h' file not found
    #include <linux/types.h>
    
    This reverts commit 85457a374fa3b1a418b47f886672f64a3d4e7bf5.
    
    Bug: 33170640
    Change-Id: I285d0b4d138897725f54180812a6c816755d4db5

--- libselinux/src/android/android_host.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-// HACK: placeholder for a library the python bindings expect.
-// Delete after b/33170640 is fixed.
-const char *selinux_openssh_contexts_path(void)
-{
-  abort();
-}
