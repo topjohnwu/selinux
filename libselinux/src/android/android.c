be36d7106882bf6819c5a14632dac4937377af37
Inseob Kim <inseob@google.com>
2023-06-16 10:46:04 +0900

    Check preinstalled app's partition
    Bug: 280547417
    Test: boot pixel and cuttlefish
    Change-Id: I6ed125eff392020ace6686514e0a102dab1fb10f
    Merged-In: I6ed125eff392020ace6686514e0a102dab1fb10f
    (cherry picked from commit dc9f3516d764207a83dbc4b08284def062ad95fb)

--- libselinux/src/android/android.c
+++ libselinux/src/android/android.c
@@ -94,11 +94,26 @@ size_t find_existing_files(
 		const path_alts_t *path_sets,
 		const char* paths[MAX_CONTEXT_PATHS])
 {
+	return find_existing_files_with_partitions(
+		path_sets,
+		paths,
+		NULL
+	);
+}
+
+size_t find_existing_files_with_partitions(
+		const path_alts_t *path_sets,
+		const char* paths[MAX_CONTEXT_PATHS],
+		const char* partitions[MAX_CONTEXT_PATHS])
+{
 	size_t i, j, len = 0;
 	for (i = 0; i < MAX_CONTEXT_PATHS; i++) {
 		for (j = 0; j < MAX_ALT_CONTEXT_PATHS; j++) {
 			const char* file = path_sets->paths[i][j];
 			if (file && access(file, R_OK) != -1) {
+				if (partitions) {
+					partitions[len] = path_sets->partitions[i];
+				}
 				paths[len++] = file;
 				/* Within each set, only the first valid entry is used */
 				break;
